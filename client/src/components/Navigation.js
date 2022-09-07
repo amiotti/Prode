ad4e3938254e18b9e0b1d7e8b723acf096d82477
Agustin MIOTTI <amiotti@secco.local>
2022-09-07 19:34:01 -0300

    Estadisticas component and minor changes addded

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -17,75 +17,77 @@ function Navigation() {
   //const { id, teams, groupMatches, getImg } = useContext(UserContext);
 
   return (
-    <>
-      <nav
-        className="navbar navbar-expand-lg navbar-light fixed-top py-3"
-        id="mainNav"
-      >
-        <div className="container px-4 px-lg-5">
-          <h3 className="navbar-brand" /*href="#page-top"*/>
-            {userLogged.name} {userLogged.lastname}
-          </h3>
-          <button
-            className="navbar-toggler navbar-toggler-right"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarResponsive"
-            aria-controls="navbarResponsive"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarResponsive">
-            <ul className="navbar-nav ms-auto my-2 my-lg-0">
-              <li className="nav-item">
-                <Link to="/">
-                  <a className="nav-link" /*href="#services"*/>Home</a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/estadisticas">
-                  <a className="nav-link" /*href="#about"*/>Estadisticas</a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/resultados">
-                  <a className="nav-link" /*href="#services"*/>Posiciones</a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/fechas">
-                  <a className="nav-link" /*href="#portfolio"*/>Fechas</a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/contacto">
-                  <a className="nav-link" /*href="#contact"*/>Contacto</a>
-                </Link>
-              </li>
+    userLogged && (
+      <>
+        <nav
+          className="navbar navbar-expand-lg navbar-light fixed-top py-3"
+          id="mainNav"
+        >
+          <div className="container px-4 px-lg-5">
+            <h3 className="navbar-brand" /*href="#page-top"*/>
+              {userLogged.name} {userLogged.lastname}
+            </h3>
+            <button
+              className="navbar-toggler navbar-toggler-right"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarResponsive"
+              aria-controls="navbarResponsive"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+              <ul className="navbar-nav ms-auto my-2 my-lg-0">
+                <li className="nav-item">
+                  <Link to="/">
+                    <a className="nav-link" /*href="#services"*/>Home</a>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/estadisticas">
+                    <a className="nav-link" /*href="#about"*/>Estadisticas</a>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/resultados">
+                    <a className="nav-link" /*href="#services"*/>Posiciones</a>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/fechas">
+                    <a className="nav-link" /*href="#portfolio"*/>Fechas</a>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/contacto">
+                    <a className="nav-link" /*href="#contact"*/>Contacto</a>
+                  </Link>
+                </li>
 
-              <li className="nav-item">
-                <Link to="/pronosticos">
-                  <a className="nav-link" /*href="#services"*/>Pronosticos</a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                {userLogged ? (
-                  <a className="nav-link" onClick={handleLogOut}>
-                    Logout
-                  </a>
-                ) : (
-                  <a className="nav-link" href="#registro">
-                    Registro
-                  </a>
-                )}
-              </li>
-            </ul>
+                <li className="nav-item">
+                  <Link to="/pronosticos">
+                    <a className="nav-link" /*href="#services"*/>Pronosticos</a>
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  {userLogged ? (
+                    <a className="nav-link" onClick={handleLogOut}>
+                      Logout
+                    </a>
+                  ) : (
+                    <a className="nav-link" href="#registro">
+                      Registro
+                    </a>
+                  )}
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
-      </nav>
-    </>
+        </nav>
+      </>
+    )
   );
 }
 
