20098f6f031b160b75d5ca946a3017510d006ca7
Agustin MIOTTI <amiotti@secco.local>
2022-09-09 18:12:47 -0300

    PlayerStats added

--- client/src/components/Estadisticas.js
+++ client/src/components/Estadisticas.js
@@ -5,6 +5,7 @@ import "../App.css";
 import AuthVerify from "../common/AuthVerify";
 import { UserContext } from "./Context";
 import { useNavigate } from "react-router-dom";
+import PlayerStats from "./PlayerStats";
 
 export default function Estadisticas() {
   const userLogged = AuthVerify();
@@ -21,18 +22,23 @@ export default function Estadisticas() {
     groupData && (
       <>
         <Navigation />
+
         <header className="masthead">
-          <h1>TABLA DE POSICIONES</h1>
           <div className="container px-4 px-lg-5 h-100">
             <div className="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
               <div className="col-lg-8 align-self-end">
+                <h1>TABLA DE POSICIONES</h1>
                 <div>
                   {groupData.map((group) => (
                     <>
-                      <h2>{group.group}</h2>
+                      <h2>{"GRUPO " + group.group.charAt(6)}</h2>
                       <GroupStats info={group} />
                     </>
                   ))}
+                </div>
+                <h1>GOLEADORES</h1>
+                <div>
+                  <PlayerStats />
                 </div>
               </div>
             </div>
